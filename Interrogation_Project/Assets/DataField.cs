f13b18170cf48511976c05a515232c6c3a1a2791
joshr2121 <joshr2121@gmail.com>
2014-06-19 11:46:52 -0400

    Started fixing conflicts

--- Interrogation_Project/Assets/DataField.cs
+++ Interrogation_Project/Assets/DataField.cs
@@ -3,21 +3,36 @@ using System.Collections;
 
 public class DataField : MonoBehaviour {
 
+	public enum DataFieldType { PlaceField, TimeField, PersonField, MeansField };
+	public DataFieldType type;
 	public bool selected;
 	public GUIText text;
 	public GUITexture texture;
 	public GUIStyle unselectedStyle;
 	public GUIStyle selectedStyle;
+	public Rect buttonRect;
+	
+	void Awake () {
+		text = gameObject.GetComponent<GUIText>();
+		texture = gameObject.GetComponent<GUITexture>();
+	}
 
 	// Use this for initialization
 	void Start () {
-		text = gameObject.GetComponent<GUIText>();
-		texture = gameObject.GetComponent<GUITexture>();
 		text.fontStyle = unselectedStyle.fontStyle;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-	}	
+	}
+	
+	void OnGUI () {
+		if (GameManager.instance.gameState == GameManager.GameState.MainMenu || GameManager.instance.gameState == GameManager.GameState.LieDetector) {
+			GUIStyle style = selected ? unselectedStyle : selectedStyle;  
+			if (GUI.Button (buttonRect,text.text, style)) {
+				GameObject.Find ("_Game_Manager").GetComponent<DataFieldManager>().Select (gameObject, type);
+			}
+		}
+	}
 }
--- Interrogation_Project/Assets/DataFieldManager.cs
+++ Interrogation_Project/Assets/DataFieldManager.cs
@@ -14,38 +14,102 @@ public class DataFieldManager : MonoBehaviour {
 	public GameObject[] meansObjects;
 
 	public int[] rightAnswers;
-
+	
 	// Use this for initialization
 	void Start () {
-		placeStrings = new string[4] {"Federal reserve", "Stock exchange", "Department of Finance", "Bank HQ"};
-		timeStrings = new string[4] {"10:00 AM", "Noon", "3:00 PM", "6:00 PM"};
-		peopleStrings = new string[4] {"Journalist", "Financier", "Government agent", "Programmer"};
-		meansStrings = new string[4] {"Cutting off communications", "Planting false information", "Shutting off the power", "Bomb threat"};
+		placeStrings = new string[4] {"Federal reserve", "Stock exchange", "Department of\nEqual Opportunity", "Liberty Bank HQ"};
+		timeStrings = new string[4] {"Monday\n1:00 PM", "Wednesday\n6:00 PM", "Thursday\n10:00 AM", "Friday\n2:00 PM"};
+		peopleStrings = new string[4] {"Mr. Wolfe\nJournalist", "Sarah Jackson\nFinancier", "Mei Yamato\nGovernment agent", "Mist\nProgrammer"};
+		meansStrings = new string[4] {"Cutting off\ncommunications", "Planting false\ninformation", "Shutting off\nthe power", "Bomb threat"};
 		
-		rightAnswers = new int[4] {0, 2, 0, 2};
+		rightAnswers = new int[4] {1, 1, 2, 1};
 		
+		/**
 		float xPos = 0.6f;
 		float xOffset = 0.1f;
 		float yPos = 0.35f;
 		float yOffset = 0.1f;
 		float zPos = 1.0f;		//In case we want to do z-ordering
+		*/
+		
+		float rectRightInset = 250f;
+		float rectY = 150f;
+		float rectWidth = 140f;
+		float rectHeight = 100f;
+		float rectXOffset = 10f;
+		float rectYOffset = 20f;
 		
 		for (int i = 0; i < 4; i++) {
+			placeObjects[i].GetComponent<DataField>().type = DataField.DataFieldType.PlaceField;
 			placeObjects[i].GetComponent<DataField>().text.text = placeStrings[i];
-			placeObjects[i].transform.position = new Vector3 (xPos + xOffset*i, yPos, zPos);
+			placeObjects[i].GetComponent<DataField>().buttonRect = new Rect (
+				Screen.width - rectRightInset - (rectWidth+rectXOffset)*i, rectY + (rectHeight+rectYOffset)*0, rectWidth, rectHeight);
+//			placeObjects[i].transform.position = new Vector3 (xPos + xOffset*i, yPos, zPos);
+			placeObjects[i].GetComponent<DataField>().text.enabled = false;
+			
+			timeObjects[i].GetComponent<DataField>().type = DataField.DataFieldType.TimeField;
 			timeObjects[i].GetComponent<DataField>().text.text = timeStrings[i];
-			timeObjects[i].transform.position = new Vector3 (xPos + xOffset*i, yPos + yOffset*1, zPos);
+			timeObjects[i].GetComponent<DataField>().buttonRect = new Rect (
+				Screen.width - rectRightInset - (rectWidth+rectXOffset)*i, rectY + (rectHeight+rectYOffset)*1, rectWidth, rectHeight);
+//			timeObjects[i].transform.position = new Vector3 (xPos + xOffset*i, yPos + yOffset*1, zPos);
+			timeObjects[i].GetComponent<DataField>().text.enabled = false;
+			
+			peopleObjects[i].GetComponent<DataField>().type = DataField.DataFieldType.PersonField;
 			peopleObjects[i].GetComponent<DataField>().text.text = peopleStrings[i];
-			peopleObjects[i].transform.position = new Vector3 (xPos + xOffset*i, yPos + yOffset*2, zPos);
+			peopleObjects[i].GetComponent<DataField>().buttonRect = new Rect (
+				Screen.width - rectRightInset - (rectWidth+rectXOffset)*i, rectY + (rectHeight+rectYOffset)*2, rectWidth, rectHeight);
+//			peopleObjects[i].transform.position = new Vector3 (xPos + xOffset*i, yPos + yOffset*2, zPos);
+			peopleObjects[i].GetComponent<DataField>().text.enabled = false;
+			
+			meansObjects[i].GetComponent<DataField>().type = DataField.DataFieldType.MeansField;
 			meansObjects[i].GetComponent<DataField>().text.text = meansStrings[i];
-			meansObjects[i].transform.position = new Vector3 (xPos + xOffset*i, yPos + yOffset*3, zPos);
+			meansObjects[i].GetComponent<DataField>().buttonRect = new Rect (
+				Screen.width - rectRightInset - (rectWidth+rectXOffset)*i, rectY + (rectHeight+rectYOffset)*3, rectWidth, rectHeight);
+			//			meansObjects[i].transform.position = new Vector3 (xPos + xOffset*i, yPos + yOffset*3, zPos);
+			meansObjects[i].GetComponent<DataField>().text.enabled = false;
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (GameManager.instance.gameState == GameManager.GameState.MainMenu || GameManager.instance.gameState == GameManager.GameState.LieDetector) {
+			for (int i = 0; i < 4; i++) {
+				placeObjects[i].GetComponent<DataField>().text.enabled = true;
+				timeObjects[i].GetComponent<DataField>().text.enabled = true;
+				peopleObjects[i].GetComponent<DataField>().text.enabled = true;
+				meansObjects[i].GetComponent<DataField>().text.enabled = true;
+			}
+		}	
 	}
+	
+	public void Select (GameObject o, DataField.DataFieldType type) {
+		switch (type) {
+		case (DataField.DataFieldType.PlaceField):
+			foreach (GameObject obj in placeObjects) {
+				if (obj != o) obj.GetComponent<DataField>().selected = false;
+				else obj.GetComponent<DataField>().selected = !obj.GetComponent<DataField>().selected;
+			}
+			break;
+		case (DataField.DataFieldType.TimeField):
+			foreach (GameObject obj in timeObjects) {
+				if (obj != o) obj.GetComponent<DataField>().selected = false;
+				else obj.GetComponent<DataField>().selected = !obj.GetComponent<DataField>().selected;
+			}
+			break;
+		case (DataField.DataFieldType.PersonField):
+			foreach (GameObject obj in peopleObjects) {
+				if (obj != o) obj.GetComponent<DataField>().selected = false;
+				else obj.GetComponent<DataField>().selected = !obj.GetComponent<DataField>().selected;
+			}
+			break;
+		case (DataField.DataFieldType.MeansField):
+			foreach (GameObject obj in meansObjects) {
+				if (obj != o) obj.GetComponent<DataField>().selected = false;
+				else obj.GetComponent<DataField>().selected = !obj.GetComponent<DataField>().selected;
+			}
+			break;
+		}
+	}	
 	
 	public bool ReportCorrect () {
 		return  placeObjects[rightAnswers[0]].GetComponent<DataField>().selected &&
